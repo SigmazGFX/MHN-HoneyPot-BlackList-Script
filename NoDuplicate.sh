74c7133a9bac614db9b548f9bf198c7d865bb5d2
DAS <46620390+wizhardhacker1@users.noreply.github.com>
2020-03-10 09:47:01 -0700

    Add files via upload
    BASH File that does the following
    
    extracts IP's from database to csv
    converts csv to txt
    Moves text to HTML location
    Sorts and removes duplicate IP's
    Deletes source txt
    Searches for IP that we want to remove (whitlisted)
    
    Create this script as BASH and the add to CRON job to run every 5 minutes and you will have realtime blacklist feed from honeypots

---
+++ NoDuplicate.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+mongoexport --db mnemosyne --collection session --fields=source_ip  --type=csv |tail -n+2 > MainList.csv
+ 
+
+
+mv MainList.csv Mainlist.txt
+
+
+
+sudo mv Mainlist.txt /var/www/html/
+
+
+
+sudo tr -d "\r" < /var/www/html/Mainlist.txt | sort -u > /var/www/html/BLackListed.txt
+
+
+
+sudo rm  /var/www/html/Mainlist.txt
+
+
+
+sudo sed -i '/xx.xx.xx.xx/d' /var/www/html/BLackListed.txt
+
+
+
+sudo sed -i '/xx.xx.xx.xx/d' /var/www/html/BLackListed.txt
+
+
+
+sudo sed -i '/xx.xx.xx.xx/d' /var/www/html/BLackListed.txt
+
+
+
+
+
+exec bash
